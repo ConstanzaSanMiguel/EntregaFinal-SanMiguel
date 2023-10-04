d818646ad5050f6504a2fffc4cc0010cb888ed27
Constanza San Miguel <csanmiguel49@gmail.com>
2023-10-03 22:23:24 -0300

    correciones navbar menu

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -11,18 +11,26 @@ const Navbar = () => {
         <>
             <Flex className='menu'>
                 <Box>
-                    <img src="src\images\vibelogo.png" alt="Vibe logo" />
+                    <img className='logoVibe' src="src\images\vibelogo.png" alt="Vibe logo" />
                 </Box>
                 <Spacer />
                 <Box className='navbar'>
                     <Menu className='nav-menu'>
                         <MenuButton className='nav-item'>
-                            Kpop albums / merch / DVD<i className="fa-solid fa-angle-down"></i>
+                            Kpop albums<i className="fa-solid fa-angle-down"></i>
                         </MenuButton>
                         <MenuList sx={basicNavbarStyle}>
                             <MenuItem sx={basicNavbarStyle} className='dropdown-content'>Shop by group</MenuItem>
                             <MenuItem sx={basicNavbarStyle} className='dropdown-content'>Shop by solo</MenuItem>
                             <MenuItem sx={basicNavbarStyle} className='dropdown-content'>Hot groups</MenuItem>
+                        </MenuList>
+                    </Menu>
+                    <Spacer />
+                    <Menu className='nav-menu'>
+                        <MenuButton className='nav-item'>
+                            Kpop merch / DVD<i className="fa-solid fa-angle-down"></i>
+                        </MenuButton>
+                        <MenuList sx={basicNavbarStyle}>
                             <MenuItem sx={basicNavbarStyle} className='dropdown-content'>Official goods</MenuItem>
                             <MenuItem sx={basicNavbarStyle} className='dropdown-content'>Official lightsticks</MenuItem>
                             <MenuItem sx={basicNavbarStyle} className='dropdown-content'>Hot sale</MenuItem>
